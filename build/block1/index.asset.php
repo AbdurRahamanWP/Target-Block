e0c85741f6792e12ed4a88af9e0676e1892f394a
Abdur Rahaman <54670007+AbdurRahamanWP@users.noreply.github.com>
2024-05-25 13:20:44 +0900

    basic block setup

---
+++ build/block1/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ea633b105217c84105d9');
---
+++ build/block2 AA/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a32889dff87e9cc4201c');
