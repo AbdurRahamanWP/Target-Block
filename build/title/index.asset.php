653edc43faeff6b55968ace1c1e6a215da23778b
Abdur Rahaman <54670007+AbdurRahamanWP@users.noreply.github.com>
2024-05-26 23:11:06 +0600

    updated

---
+++ build/title/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '86d18fe8d6499797dcc9');
---
+++ build/title/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a5e20eac5f4cfdd64ae0');
